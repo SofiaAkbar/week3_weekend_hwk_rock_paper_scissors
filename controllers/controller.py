be6131890a41e4467b3817d71d2cd6965ed58cde
SofiaAkbar <sofiaakbar@hotmail.co.uk>
2021-08-08 14:37:15 +0100

    basic game works except drawing

--- controllers/controller.py
+++ controllers/controller.py
@@ -1,3 +1,15 @@
 from flask import render_template, request, redirect
 from app import app
+from models.game import *
+from models.player import *
+
+@app.route('/<string:choice1>/<string:choice2>')
+def new_game(choice1, choice2):
+    player1 = Player("player1", choice1)
+    player2 = Player("player2", choice2)
+    game = Game(player1, player2)
+    winner = game.find_winner()
+    return render_template('base.html', title='Game', winner=winner)
+
+
 
---
+++ models/tests.py
@@ -0,0 +1,48 @@
+# class Player():
+#     def __init__(self, name, choice):
+#         self.name = name
+#         self.choice = choice
+
+# # =======================
+
+# class Game():
+#     def __init__(self, player1, player2):
+#         self.player1 = player1
+#         self.player2 = player2
+
+#     def winning_choice(self, choice1, choice2):
+#         if choice1 == choice2:
+#             return None
+#         elif choice1 == "rock" and choice2 == "paper":
+#             return choice1
+#         elif choice1 == "rock" and choice2 == "scissors":
+#             return choice1
+#         elif choice1 == "scissors" and choice2 == "rock":
+#             return choice2
+#         elif choice1 == "scissors" and choice2 == "paper":
+#             return choice1
+#         elif choice1 == "paper" and choice2 == "rock":
+#             return choice2
+#         elif choice1 == "paper" and choice2 == "scissors":
+#             return choice2
+#         else:
+#             return None
+        
+        
+#     def find_winner(self):
+#         winning_choice = self.winning_choice(self.player1.choice, self.player2.choice)
+#         if self.player1.choice == winning_choice:
+#             return self.player1
+#         elif self.player2.choice == winning_choice:
+#             return self.player2
+#         else:
+#             return None
+
+# # =======================
+
+# game = Game(
+#     Player("personA", "rock"), 
+#     Player("personB", "paper")
+# )
+
+# print(game.find_winner().name)
